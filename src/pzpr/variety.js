32d449a83ae22e02357b457d2fe8b6dfa9da09cd
Danlson <danlson83@gmail.com>
2026-03-13 11:40:50 -0400

    Create weirdloop puzzle variety registration

---
+++ src/pzpr/variety.js
@@ -0,0 +1,10 @@
+// Registering weirdloop puzzle variety
+
+var Weirdloop = {
+    id: 'weirdloop',
+    name: 'Weirdloop',
+    description: 'A unique puzzle variety that challenges logical thinking and pattern recognition.',
+    // Additional properties and methods can be added here
+};
+
+module.exports = Weirdloop;
